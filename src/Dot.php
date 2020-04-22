1d221377e904fafdc9d3250cf9816b1acb17a673
Jonathan Taylor <jonny.taylor5@gmail.com>
2020-04-22 14:36:19 +0100

    Setup project

---
+++ src/Dot.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Fabrica\Dot;
+
+class Dot
+{
+	private $data;
+
+	public function __construct($data)
+	{
+		$this->data = $data;
+	}
+
+	public static function from(&$data): self
+	{
+		return new self($data);
+	}
+
+	public function get(string $path)
+	{
+		list($item, $key) = $this->parse($this->data, $path);
+
+		if (is_array($item)) {
+			return $item[$key];
+		}
+	}
+
+	public function set(string $path, $value)
+	{
+		$return = $this->parse($this->data, $path);
+		$item = &$return[0];
+		$key = $return[1];
+
+		if (is_array($item)) {
+			$item[$key] = $value;
+		}
+	}
+
+	private function parse(&$data, string $path)
+	{
+		$segments = explode('.', $path);
+		return $this->traverse($data, $segments);
+	}
+
+	private function traverse(&$data, array $segments): array
+	{
+		$key = array_shift($segments);
+
+		if (count($segments) === 0) {
+			return [&$data, $key];
+		}
+
+		if (is_array($data)) {
+			$data = &$data[$key];
+		}
+
+		return $this->traverse($data, $segments);
+	}
+}
---
+++ test/DotTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace Fabrica\Dot\Test;
+
+use Fabrica\Dot\Dot;
+use PHPUnit\Framework\TestCase;
+
+class DotTest extends TestCase
+{
+	/** @test */
+	public function it_can_get_path_through_arrays()
+	{
+		$data = [
+			'nested' => [
+				'data' => [
+					'property' => 'value'
+				]
+			]
+		];
+
+		$value = Dot::from($data)->get('nested.data.property');
+		self::assertEquals('value', $value);
+	}
+
+	/** @test */
+	public function it_can_set_path_through_arrays()
+	{
+		$data = [
+			'nested' => [
+				'data' => [
+					'property' => 'value'
+				]
+			]
+		];
+
+		$dot = Dot::from($data);
+		$dot->set('nested.data.property', 'foo');
+		self::assertEquals('foo', $dot->get('nested.data.property'));
+	}
+}
